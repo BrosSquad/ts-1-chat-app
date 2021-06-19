253a61f1d585db532c4337ce04191ed1200be637
Jaksa Malisic <jaksa.malisic@gmail.com>
2021-06-19 22:46:23 +0200

    Add chat UI

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -31,6 +31,41 @@ List<MessageResponse> messages = [
     text: 'Ne seeeeri',
     user: userA,
   ),
+  MessageResponse(
+    createdAt: 'Sat, 19 Jun 2021 20:16:24 +0200',
+    text: 'Ne seeeeri',
+    user: userA,
+  ),
+  MessageResponse(
+    createdAt: 'Sat, 19 Jun 2021 20:16:24 +0200',
+    text: 'Ne seeeeri',
+    user: userA,
+  ),
+  MessageResponse(
+    createdAt: 'Sat, 19 Jun 2021 20:16:24 +0200',
+    text: 'Ne seeeeri',
+    user: userA,
+  ),
+  MessageResponse(
+    createdAt: 'Sat, 19 Jun 2021 20:16:24 +0200',
+    text: 'Ne seeeeri',
+    user: userA,
+  ),
+  MessageResponse(
+    createdAt: 'Sat, 19 Jun 2021 20:16:24 +0200',
+    text: 'Ne seeeeri',
+    user: userA,
+  ),
+  MessageResponse(
+    createdAt: 'Sat, 19 Jun 2021 20:16:24 +0200',
+    text: 'Ne seeeeri',
+    user: userA,
+  ),
+  MessageResponse(
+    createdAt: 'Sat, 19 Jun 2021 20:16:24 +0200',
+    text: 'Ne seeeeri',
+    user: userA,
+  ),
 ];
 
 class ChatScreen extends StatefulWidget {
@@ -41,6 +76,22 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void sendMessage() {}
+
   @override
   Widget build(BuildContext context) {
     String username = GRPCClient.of(context).authService.username;
@@ -50,48 +101,91 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text(username),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: messages.length,
-            shrinkWrap: true,
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              MessageResponse message = messages[index];
-              return Container(
-                child: Align(
-                  alignment: message.user.username == 'Misko'
-                      ? Alignment.topRight
-                      : Alignment.topLeft,
-                  child: Container(
-                    constraints: BoxConstraints(
-                      minWidth: 184.0,
-                      maxWidth: 284.0,
-                    ),
-                    padding: EdgeInsets.all(16),
-                    margin: EdgeInsets.symmetric(vertical: 4),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16.0),
-                      color: message.user.username == 'Misko'
-                          ? Theme.of(context).primaryColorLight
-                          : Theme.of(context).primaryColor,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(message.text),
-                        SizedBox(height: 4.0),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text(timeago.format(DateTime.now())),
-                        ),
-                      ],
-                    ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: sendMessage,
+        child: Icon(Icons.send),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemCount: messages.length,
+                  shrinkWrap: true,
+                  reverse: true,
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  physics: AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (context, index) =>
+                      MessageBubble(message: messages[index]),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                height: 84.0,
+                padding: EdgeInsets.only(top: 16),
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                constraints: BoxConstraints(
+                  maxWidth: 324,
+                ),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    filled: true,
                   ),
                 ),
-              );
-            }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
+
+  final MessageResponse message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Align(
+        alignment: message.user.username == 'Misko'
+            ? Alignment.topRight
+            : Alignment.topLeft,
+        child: Container(
+          constraints: BoxConstraints(
+            minWidth: 184.0,
+            maxWidth: 284.0,
+          ),
+          padding: EdgeInsets.all(16),
+          margin: EdgeInsets.symmetric(vertical: 4),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.0),
+            color: message.user.username == 'Misko'
+                ? Theme.of(context).primaryColorLight
+                : Theme.of(context).primaryColor,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(message.text),
+              SizedBox(height: 4.0),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Text(timeago.format(DateTime.now())),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
